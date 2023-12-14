b6e412aee34ae5e08bbaf4e864f45c581d6de1bd
RedStarZ77 <redstarz2002@mail.ru>
2023-12-14 19:43:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import requests
+from flask import Flask, render_template, request
+from mysql.connector import connect, Error
+import os
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/', methods=['GET','POST'])
+def root():
+    result = None
+
+    try:
+        num1 = float(request.args.get('num1'))
+        num2 = float(request.args.get('num2'))
+        operation = request.args.get('operation')
+    except Exception as e:
+        result = f"Error: {str(e)}"
+    try:
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                result = "Error: Division by zero"
+    except Exception as e:
+        result = f"Error: {str(e)}"
+    return {"result":float(result),"num1":float(num1),"num2":float(num2)}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3001)
