9fb5c3a3d2d9c89f49d3a35a203577ea6969f44a
RedStarZ77 <redstarz2002@mail.ru>
2023-12-18 19:09:07 +0300

    Update component_test.py

--- component_test.py
+++ component_test.py
@@ -6,4 +6,4 @@ base_url = 'http://localhost:3001/'
 
 def test_root_get():
     res = requests.get(base_url+'?num1=2&num2=2&operation=add')
-    assert '{"num1":2.0,"num2":2.0,"result":4.0}' in res.text
+    assert ("result" in res.text) == True
