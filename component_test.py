c74a56dd4c75a64d01b9f0c3faad8fd5e2fe938c
RedStarZ77 <redstarz2002@mail.ru>
2023-12-18 19:16:29 +0300

    Com 12

--- component_test.py
+++ component_test.py
@@ -5,5 +5,5 @@ base_url = 'http://localhost:3001/'
 
 
 def test_root_get():
-    res = requests.get(base_url+'?num1=2&num2=2&operation=add')
-    assert ("result" in res.text) == True
+    res = requests.get(base_url+'?num1=2&num2=2&operation=add').json()
+    assert 'result' in res
