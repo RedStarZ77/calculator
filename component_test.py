51a9207ce7010f69224c530aa96737c1098d1e42
RedStarZ77 <redstarz2002@mail.ru>
2023-12-18 18:29:46 +0300

    Com 10

--- component_test.py
+++ component_test.py
@@ -1,9 +1,9 @@
 import requests
-import json
 
 
 base_url = 'http://localhost:3001/'
 
+
 def test_root_get():
-    res = requests.get(base_url+'/?num1=2&num2=2&operation=add')
-    assert res.json()['result'] == 4
+    res = requests.get(base_url+'?num1=2&num2=2&operation=add')
+    assert res.json()["result"] == 4
